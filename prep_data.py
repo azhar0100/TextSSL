437675ca911bc45101122ee226cb245789613f3b
azhar0100 <azhartahir0100@gmail.com>
2022-06-16 15:37:15 +0500

    Added consistent vocabulary forming script

--- prep_data.py
+++ prep_data.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
 import nltk
 from os.path import join, exists
 import re
@@ -89,22 +90,21 @@ def clean_doc(string, dataset):
         string = clean_doc_ap(string)
     else:
         pass
-    string = re.sub(r"^\"", "", string)
-    string = re.sub(r"\"$", "", string)
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
     string = re.sub(r"\'re", " \'re", string)
     string = re.sub(r"\'d", " \'d", string)
     string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r"\.", " ", string)
-    string = re.sub(r",", " ", string)
-    string = re.sub(r"!", " ", string)
-    string = re.sub(r"\(", " ", string)
-    string = re.sub(r"\)", " ", string)
-    string = re.sub(r"\?", " ", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
+
 
 
 import argparse
@@ -224,5 +224,5 @@ if __name__ == '__main__':
         f.write(doc_text)
     
     with open(output_folder/'vocab_english_data.clean.txt','w') as f:
-        vocab = '\n'.join(list(set(doc_text.split())))
+        vocab = '\n'.join(list(set(doc_text.split() )))
         f.write(vocab)
